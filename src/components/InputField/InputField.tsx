9471a0c498fd5aee03d9637a70ba6439e0d2f8c6
Nicolás Leal <lealnicolas96@gmail.com>
2023-04-04 14:37:27 -0400

    Using Joi schema. Update InputField component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -3,27 +3,38 @@ import classNames from 'classnames';
 
 interface IInputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   hasErrors?: boolean;
+  leadIcon?: React.ReactNode;
 }
 
-export function InputField({ hasErrors, ...inputProps }: IInputFieldProps) {
+export function InputField({
+  hasErrors,
+  leadIcon,
+  ...inputProps
+}: IInputFieldProps) {
   return (
-    <>
+    <div className='relative flex'>
+      {!!leadIcon && (
+        <span className='absolute left-0 top-0 inline-flex h-full max-w-[45px] items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-500'>
+          {leadIcon}
+        </span>
+      )}
       <input
+        type='text'
+        id='website-admin'
         className={classNames(
-          'w-full',
-          { 'border border-gray-300 text-gray-900': !hasErrors },
-          { 'border border-red-300 text-red-500': hasErrors },
+          'block h-10 w-full min-w-0 flex-1 cursor-pointer bg-white text-sm font-normal leading-6 shadow-sm ring-0 ring-offset-0 placeholder:text-gray-400 focus:ring-0',
           {
+            'border border-gray-300 text-gray-900': !hasErrors,
+            'border border-red-300 text-red-500 placeholder:text-red-300':
+              hasErrors,
             'cursor-not-allowed border border-gray-300 text-gray-400':
               inputProps.disabled,
-          },
-          'text-base font-normal leading-6',
-          'h-10 cursor-pointer rounded bg-white px-2.5 py-3 shadow-sm',
-          'placeholder:text-gray-400',
-          'focus:outline-none focus:ring focus:ring-indigo-300'
+            'rounded px-2.5 py-3 ': !leadIcon,
+            'rounded-md py-3 pl-[55px] pr-2.5': !!leadIcon,
+          }
         )}
         {...inputProps}
       />
-    </>
+    </div>
   );
 }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,59 +1,67 @@
 import { useForm, Controller } from 'react-hook-form';
 import type { SubmitHandler } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
+import { AtSymbolIcon } from '@heroicons/react/24/solid';
 import { LoginFormValues } from '../../types/LoginFormValues';
 import { InputField } from '../../components/InputField/InputField';
 import { InputError } from '../../components/InputError/InputError';
 import { Button } from '../../components/Button/Button';
+import { loginSchema } from '../../lib/rules';
+import { joiResolver } from '@hookform/resolvers/joi';
 
 export function LoginPage() {
   const {
     handleSubmit,
     control,
     formState: { errors, isValid, isSubmitted },
-  } = useForm<LoginFormValues>();
+  } = useForm<LoginFormValues>({
+    resolver: joiResolver(loginSchema),
+  });
 
   const onSubmit: SubmitHandler<LoginFormValues> = (data) => {
     console.log(data);
   };
 
   return (
-    <section className='grid h-screen w-full grid-cols-1 place-content-center bg-gray-200 px-8'>
-      <h1 className='mb-6 block w-full border-b border-indigo-500 pb-3 text-center text-2xl font-bold text-gray-400'>
-        Digital Tech Inc.
-      </h1>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col rounded border border-dashed border-indigo-400 bg-white p-10'
-      >
-        <div className='flex items-center'>
-          <Controller
-            control={control}
-            name='username'
-            rules={{
-              required: 'Debes ingresar un nombre de usuario válido',
-            }}
-            render={({ field: { value, name, onChange } }) => (
-              <InputField
-                name={name}
-                value={value ?? ''}
-                onChange={onChange}
-                hasErrors={!!errors.username?.message}
-              />
-            )}
-          />
+    <section className='h-screen w-full bg-gray-200'>
+      <div className='container grid h-full w-full grid-cols-1 place-content-center px-8 md:mx-auto md:w-3/6 xl:w-2/6'>
+        <h1 className='mb-6 block w-full border-b border-indigo-500 pb-3 text-center text-2xl font-bold text-gray-400'>
+          Digital Tech Inc.
+        </h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='flex flex-col items-center rounded border border-dashed border-indigo-400 bg-white p-10'
+        >
+          <div className='flex items-center'>
+            <Controller
+              control={control}
+              name='username'
+              render={({ field: { value, name, onChange } }) => (
+                <InputField
+                  name={name}
+                  value={value ?? ''}
+                  onChange={onChange}
+                  hasErrors={!!errors.username?.message}
+                  placeholder='john_doe23'
+                  leadIcon={<AtSymbolIcon className='h-5 w-5 text-inherit' />}
+                />
+              )}
+            />
 
-          <Button type='submit' disabled={!isValid && isSubmitted}>
-            Entrar
-          </Button>
-        </div>
+            <Button type='submit' disabled={!isValid && isSubmitted}>
+              Entrar
+            </Button>
+          </div>
 
-        <ErrorMessage
-          errors={errors}
-          name='username'
-          render={({ message }) => <InputError message={message} />}
-        />
-      </form>
+          <div className='w-full'>
+            <ErrorMessage
+              errors={errors}
+              name='username'
+              render={({ message }) => <InputError message={message} />}
+            />
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
