8ef2b424f6eb48fa9951d51a28669a166d224161
Nicolás Leal <lealnicolas96@gmail.com>
2023-04-05 13:24:17 -0400

    Install and config dayjs dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { locale, extend } from 'dayjs';
+import 'dayjs/locale/es';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { MainRouter } from './pages/MainRouter';
 import './index.css';
+
+locale('es');
+extend(relativeTime);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
