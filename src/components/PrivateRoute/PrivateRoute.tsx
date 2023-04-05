1095dd4b6d7294e3c7ba22b2ed4b13fb14e6a91f
Nicolás Leal <lealnicolas96@gmail.com>
2023-04-05 00:36:00 -0400

    Fix public and private route component behavior

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
+import { LOGIN_URL } from '../../lib/routes';
+import { useAuth } from '../../hooks/useAuth';
 
 interface IPrivateRouteProps {
   children: ReactElement;
 }
 
 export function PrivateRoute({ children }: IPrivateRouteProps) {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    return <Navigate to={LOGIN_URL} />;
+  }
+
   return children;
 }
--- src/components/PublicRoute/PublicRoute.tsx
+++ src/components/PublicRoute/PublicRoute.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
+import { FEED_URL } from '../../lib/routes';
+import { useAuth } from '../../hooks/useAuth';
 
 interface IPublicRouteProps {
   children: ReactElement;
 }
 
 export function PublicRoute({ children }: IPublicRouteProps) {
+  const { isLoggedIn } = useAuth();
+
+  if (isLoggedIn) {
+    return <Navigate to={FEED_URL} />;
+  }
+
   return children;
 }
